2503530c6315261225a5c8cf9584d3c571fd922d
ecolotta <75556579+ecolotta@users.noreply.github.com>
2021-06-11 11:16:26 +0900

    Revert "fix:vendor/bundleをgitignoreに追加する"

--- vendor/bundle/ruby/2.6.0/bundler/gems/rspec-rails-bfa37ce6d6ab/spec/rspec/rails/example/model_example_group_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-module RSpec::Rails
-  describe ModelExampleGroup do
-    it_behaves_like "an rspec-rails example group mixin", :model,
-      './spec/models/', '.\\spec\\models\\'
-  end
-end
